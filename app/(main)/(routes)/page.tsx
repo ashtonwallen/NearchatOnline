946fd131103a3acbb0447a8e4b599e1fa89cd383
ashtonwilliamallen <aashton482@gmail.com>
2024-01-18 16:19:16 -0500

    Add simple user auth. Google/email

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
-
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      This is a protected route
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/" />
+      Caught a vibe
     </div>
   )
 }
